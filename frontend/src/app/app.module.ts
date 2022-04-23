877d6c2ec432e341ec5a337d4e1b2d4c785369d3
panopticonism <panopticonism@gmail.com>
2022-04-23 20:01:51 +0200

    PWA installed.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -26,6 +26,12 @@ import { environment } from '../environments/environment';
       enabled: environment.production,
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
