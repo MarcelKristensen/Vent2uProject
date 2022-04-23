19d867706b15f0735823afcc2f20ce800569b518
alerion-vlad <vlad.polec@gmail.com>
2022-04-23 16:33:10 +0200

    Created Random Number Generator

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from 'src/app/services/crud.service';
 
+var Random = function () {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  
   rooms: any = [];
   zones: any = [];
   roomZones: any = [];
@@ -23,7 +30,11 @@ export class HomePage implements OnInit {
       this.zones = Object.values(res).map((zone) => zone.zone);
       this.getUniqueLocation();
     });
+    const UserId = '#' + Math.random().toString(36).slice(2, 9);
+
   }
+
+
 
   getUniqueLocation() {
     const freeZone = this.roomZones.find((roomZone) => roomZone !== this.zones);
