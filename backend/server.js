9ada5192b37b586144e7aa7273cfed2201493b91
panopticonism <panopticonism@gmail.com>
2022-04-25 18:50:15 +0200

    Ports added for online server.

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const { rooms } = require("./models/rooms.json");
 
 const corsOptions = {
-  origin: "http://localhost:3316",
+  origin: `http://localhost:${process.env.PORT || 3306}`,
 };
 app.use(cors(corsOptions));
 
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 require("./routes/room.routes")(app);
 require("./routes/user_input.routes")(app);
 
-const PORT = 3316;
+const PORT = process.env.PORT || 3316;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
