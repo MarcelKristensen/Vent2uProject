055e5275ca88dc97f28a7a0cf5e82c1047e5ef8a
Marcel Kristensen <marcel@geoprof.dk>
2022-05-05 19:12:03 +0200

    Userinput Post Request

--- frontend/src/app/pages/questionnaire/questionnaire.page.ts
+++ frontend/src/app/pages/questionnaire/questionnaire.page.ts
@@ -8,36 +8,13 @@ import { Storage } from '@ionic/storage-angular';
   styleUrls: ['./questionnaire.page.scss'],
 })
 export class QuestionnairePage implements OnInit {
-  userinput = {
-    gender: '',
-    temperature: '',
-    energy: '',
-    humidity: ''
-  };
+
   submitted = false;
   private _storage: Storage | null = null;
 
   constructor(
-    private userinputService: UserinputService,
-    private storage: Storage
+    public storage: Storage
     ) {
-  }
-  saveUserinput() {
-    const data = {
-      gender: this.userinput.gender,
-      temperature: this.userinput.temperature,
-      energy: this.userinput.energy,
-      humidity: this.userinput.humidity
-    };
-    this.userinputService.createUserinput(data)
-    .subscribe(
-      response => {
-        console.log(response);
-        this.submitted = true;
-      },
-      error => {
-        console.log(error);
-      });
   }
 
   async ngOnInit() {
