192b73ea7b009e85186637c17baf55cfa73ad7ee
Nikolaj <niko9946@edu.zealand.dk>
2022-05-05 15:24:01 +0200

    Get room (filtered), get zones (not filtered)

--- frontend/src/app/pages/admin/admin.page.ts
+++ frontend/src/app/pages/admin/admin.page.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { element } from 'protractor';
 import { CrudService } from 'src/app/services/crud.service';
-import { Room } from 'src/app/services/room';
-
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.page.html',
@@ -11,26 +8,53 @@ import { Room } from 'src/app/services/room';
 export class AdminPage implements OnInit {
   rooms: any = [];
   zones: any = [];
-  uniqueRoom = [];
+  getRooms = [];
+  getZones = [];
+  getSpecificZone = [];
+  selectedRoom: '';
+  selectedZone: number;
 
   constructor(private crudService: CrudService) {}
-
   ngOnInit() {
     this.crudService.getRooms().subscribe((res) => {
       this.rooms = res;
       this.getRoomNames();
     });
+
+    this.crudService.getRooms().subscribe((res) => {
+      this.zones = res;
+      this.getRoomZones();
+    });
   }
 
   getRoomNames() {
     let roomName = [];
-
     for (let index = 0; index < this.rooms.length; index++) {
       const element = this.rooms[index];
       roomName.push(element.name);
     }
-    this.uniqueRoom = roomName.filter(
+    this.getRooms = roomName.filter(
       (element, index) => roomName.indexOf(element) === index
+    );
+  }
+
+  getRoomZones() {
+    if (!this.selectedRoom) {
+      console.log('no room selected');
+      //Do nothing
+    } else {
+      console.log('room has been selected!');
+      //Get zones related to the selected room
+    }
+
+    let roomZone = [];
+    for (let index = 0; index < this.zones.length; index++) {
+      const element = this.zones[index];
+      roomZone.push(element.zone);
+      //console.log(element.zone);
+    }
+    this.getZones = roomZone.filter(
+      (element, index) => roomZone.indexOf(element) === index
     );
   }
 }
