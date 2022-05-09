4e1166f95b164300d2b295026ee4b5ffeb0d328c
Nikolaj <niko9946@edu.zealand.dk>
2022-05-09 11:40:02 +0200

    get rooms and zones (only room is filtered)

--- frontend/src/app/pages/admin/admin.page.ts
+++ frontend/src/app/pages/admin/admin.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from 'src/app/services/crud.service';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.page.html',
@@ -10,9 +11,11 @@ export class AdminPage implements OnInit {
   zones: any = [];
   getRooms = [];
   getZones = [];
-  getSpecificZone = [];
-  selectedRoom: '';
-  selectedZone: number;
+
+  //used in the html for the selected values
+  //Like selecting D3.05 and zone 3 to work with the user values in the ts file.
+  selectedRoom: string = '';
+  selectedZone: string = '';
 
   constructor(private crudService: CrudService) {}
   ngOnInit() {
@@ -39,19 +42,11 @@ export class AdminPage implements OnInit {
   }
 
   getRoomZones() {
-    if (!this.selectedRoom) {
-      console.log('no room selected');
-      //Do nothing
-    } else {
-      console.log('room has been selected!');
-      //Get zones related to the selected room
-    }
-
     let roomZone = [];
     for (let index = 0; index < this.zones.length; index++) {
       const element = this.zones[index];
       roomZone.push(element.zone);
-      //console.log(element.zone);
+      console.log(element.zone);
     }
     this.getZones = roomZone.filter(
       (element, index) => roomZone.indexOf(element) === index
