e92a734b99175c0c22bf6c95749c4d1546f67246
Nikolaj <niko9946@edu.zealand.dk>
2022-05-13 11:13:23 +0200

    Can fetch data (not filtered)

--- frontend/src/app/pages/admin/admin.page.ts
+++ frontend/src/app/pages/admin/admin.page.ts
@@ -1,5 +1,7 @@
-import { RoomService } from 'src/app/services/room.service';
 import { Component, OnInit } from '@angular/core';
+import { RoomService } from 'src/app/services/room.service';
+import { UserinputService } from 'src/app/services/userinput.service';
+import { ZoneService } from 'src/app/services/zone.service';
 
 @Component({
   selector: 'app-admin',
@@ -8,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPage implements OnInit {
   rooms: any[];
+  zones: any[];
   userinputs: any[];
   openform = false;
-  constructor(public roomService: RoomService) {
+  constructor(
+    public roomService: RoomService,
+    public zoneService: ZoneService,
+    public userService: UserinputService
+  ) {
     this.getAllRooms();
+    this.getAllZones();
+    this.getAllUserInputs();
   }
 
   getAllRooms() {
@@ -19,6 +28,30 @@ export class AdminPage implements OnInit {
       (res: any) => {
         console.log('SUCCESS ====', res);
         this.rooms = res;
+      },
+      (error: any) => {
+        console.log('ERRROR ===', error);
+      }
+    );
+  }
+
+  getAllZones() {
+    this.zoneService.getAllZones().subscribe(
+      (res: any) => {
+        console.log('SUCCESS ====', res);
+        this.zones = res;
+      },
+      (error: any) => {
+        console.log('ERRROR ===', error);
+      }
+    );
+  }
+
+  getAllUserInputs() {
+    this.userService.getAllUserInputs().subscribe(
+      (res: any) => {
+        console.log('SUCCESS ====', res);
+        this.userinputs = res;
       },
       (error: any) => {
         console.log('ERRROR ===', error);
