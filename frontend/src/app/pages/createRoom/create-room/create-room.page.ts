16f5e247cfc8ee096a9c75dd3ca83360dca9397b
panopticonism <panopticonism@gmail.com>
2022-05-17 22:49:45 +0200

    Validation added to form.

--- frontend/src/app/pages/createRoom/create-room/create-room.page.ts
+++ frontend/src/app/pages/createRoom/create-room/create-room.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-room',
@@ -7,10 +7,19 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./create-room.page.scss'],
 })
 export class CreateRoomPage implements OnInit {
+  numberRegEx = /\-?\d*\.?\d{1,2}/;
   formRoom = new FormGroup({
-    name: new FormControl(),
-    startZone: new FormControl(),
-    endZone: new FormControl(),
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    startZone: new FormControl('', [
+      Validators.required,
+      Validators.pattern(this.numberRegEx),
+      Validators.maxLength(2),
+    ]),
+    endZone: new FormControl('', [
+      Validators.required,
+      Validators.pattern(this.numberRegEx),
+      Validators.maxLength(2),
+    ]),
   });
 
   constructor() {}
