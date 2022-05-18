b4900f5248d945cc9016adf5eaa9e8ca17004d36
panopticonism <panopticonism@gmail.com>
2022-05-18 19:13:50 +0200

    Delete button functional.

--- frontend/src/app/pages/editRooms/edit-rooms/edit-rooms.page.ts
+++ frontend/src/app/pages/editRooms/edit-rooms/edit-rooms.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ZoneService } from 'src/app/services/zone.service';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-edit-rooms',
   templateUrl: './edit-rooms.page.html',
@@ -9,12 +9,56 @@ import { ZoneService } from 'src/app/services/zone.service';
 export class EditRoomsPage implements OnInit {
   zones;
 
-  constructor(private zoneService: ZoneService) {}
+  constructor(
+    private zoneService: ZoneService,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {
     this.zoneService.getAllZones().subscribe((res) => {
       this.zones = res;
       console.log(this.zones);
     });
+  }
+
+  async alertDeleteZoneEntry(zone) {
+    console.log(zone.id);
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      message: `Are you sure you want to delete entry with zone ${zone.number}?`,
+      buttons: [
+        {
+          text: 'Return',
+          role: 'cancel',
+          cssClass: 'secondary',
+          id: 'cancel-button',
+          handler: () => {
+            console.log('Cancel delete');
+          },
+        },
+        {
+          text: 'Confirm',
+          id: 'confirm-button',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.deleteZoneEntry(zone.id);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  deleteZoneEntry(zoneId) {
+    this.zoneService.deleteZone(zoneId).subscribe(
+      (res) => {
+        console.log('SUCCESS ===', res);
+        this.ngOnInit();
+      },
+      (error: any) => {
+        console.log('ERROR ===', error);
+      }
+    );
   }
 }
